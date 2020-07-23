311f2c803b433d468acf541a3939af5416b1f10e
mhrxad <mhrxadev@gmail.com>
2020-07-23 20:40:49 +0430

    Add JsonResponseStatus

---
+++ Jupiter.Core/Utilities/Common/JsonResponseStatus.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jupiter.Core.Utilities.Common
+{
+    public static class JsonResponseStatus
+    {
+        public static JsonResult Success()
+        {
+            return new JsonResult(new { status = "Success" });
+        }
+
+        public static JsonResult Success(object returnData)
+        {
+            return new JsonResult(new { status = "Success", data = returnData });
+        }
+
+        public static JsonResult NotFound()
+        {
+            return new JsonResult(new { status = "NotFound" });
+        }
+
+        public static JsonResult NotFound(object returnData)
+        {
+            return new JsonResult(new { status = "NotFound", data = returnData });
+        }
+
+        public static JsonResult Error()
+        {
+            return new JsonResult(new { status = "Error" });
+        }
+
+        public static JsonResult Error(object returnData)
+        {
+            return new JsonResult(new { status = "Error", data = returnData });
+        }
+    }
+
+
+}
